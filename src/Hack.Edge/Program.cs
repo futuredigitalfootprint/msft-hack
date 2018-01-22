43c8fe601a87a6a86cd33fcda650d4072ac53711
Tommy Long <pete_201@hotmail.com>
2018-01-22 12:35:58 +0000

    added lightweight edge api startupl

---
+++ src/Hack.Edge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Hack.Edge
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            new WebHostBuilder()
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+	            .ConfigureAppConfiguration((context, config) => config
+		            .AddJsonFile("appsettings.json")
+		            .AddUserSecrets<Program>()
+		            .AddEnvironmentVariables()
+	            )
+				.ConfigureLogging((context, logging) => logging
+		            .AddDebug()
+		            .AddConsole())
+				.UseStartup<Startup>()
+                .Build();
+    }
+}
